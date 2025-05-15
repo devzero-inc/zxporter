dbbd03d4c2a0c4c6c15274eb47017b4aa9b1f051
kevin <kevin@devzero.io>
2025-05-15 15:10:59 -0700

    cleanup metadata infer

--- internal/collector/provider/detector.go
+++ internal/collector/provider/detector.go
@@ -45,48 +45,8 @@ func NewDetector(logger logr.Logger, k8sClient kubernetes.Interface, opts ...Det
 
 // DetectProvider attempts to identify and return the appropriate provider
 func (d *Detector) DetectProvider(ctx context.Context) (Provider, error) {
-	d.logger.Info("Detecting cloud provider")
-
-	// Try AWS EKS
-	d.logger.Info("Attempting to detect AWS EKS provider")
-	awsProvider, err := NewAWSProvider(d.logger, d.k8sClient)
-	if err == nil {
-		// Try to get metadata to confirm this is AWS
-		_, err = awsProvider.GetClusterMetadata(ctx)
-		if err == nil {
-			d.logger.Info("Detected AWS EKS provider")
-			return awsProvider, nil
-		}
-		d.logger.Info("AWS detection failed", "error", err)
-	}
-
-	// Try GCP GKE
-	d.logger.Info("Attempting to detect GCP GKE provider")
-	gcpProvider, err := NewGCPProvider(d.logger, d.k8sClient)
-	if err == nil {
-		// Try to get metadata to confirm this is GCP
-		_, err = gcpProvider.GetClusterMetadata(ctx)
-		if err == nil {
-			d.logger.Info("Detected GCP GKE provider")
-			return gcpProvider, nil
-		}
-		d.logger.Info("GCP detection failed", "error", err)
-	}
-
-	// Try Azure AKS
-	d.logger.Info("Attempting to detect Azure AKS provider")
-	azureProvider, err := NewAzureProvider(d.logger, d.k8sClient)
-	if err == nil {
-		// Try to get metadata to confirm this is Azure
-		_, err = azureProvider.GetClusterMetadata(ctx)
-		if err == nil {
-			d.logger.Info("Detected Azure AKS provider")
-			return azureProvider, nil
-		}
-		d.logger.Info("Azure detection failed", "error", err)
-	}
 
 	// Fall back to generic provider
 	d.logger.Info("No specific cloud provider detected, using generic provider")
-	return NewGenericProvider(d.logger, d.k8sClient), nil
+	return NewGenericProvider(d.logger, d.k8sClient, d.kubeContextName), nil
 }
--- internal/collector/provider/generic.go
+++ internal/collector/provider/generic.go
@@ -7,58 +7,50 @@ import (
 	"sync"
 
 	"github.com/go-logr/logr"
+	"go.uber.org/zap"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
 const (
-	// Common role labels
-	nodeRoleWorkerLabel    = "node-role.kubernetes.io/worker"
-	nodeRoleMasterLabel    = "node-role.kubernetes.io/master"
-	kubernetesRoleLabel    = "kubernetes.io/role"
-	nodeInstanceGroupLabel = "node.kubernetes.io/instance-group"
-	nodeGroupLabel         = "node-group"
+	providerNameUnknown      = "unknown"
+	providerNameAws          = "aws"
+	providerNameGcp          = "gcp"
+	providerNameAzure        = "azure"
+	providerNameOpenstack    = "openstack"
+	providerNameDigitalocean = "digitalocean"
 
-	// Instance type labels
-	nodeInstanceTypeLabel     = "node.kubernetes.io/instance-type"
-	nodeBetaInstanceTypeLabel = "beta.kubernetes.io/instance-type"
+	nodeProviderPrefixAws          = "aws://"
+	nodeProviderPrefixGcp          = "gce://"
+	nodeProviderPrefixAzure        = "azure://"
+	nodeProviderPrefixOpenstack    = "openstack://"
+	nodeProviderPrefixDigitalocean = "digitalocean://"
 
-	// Zone labels
-	topologyZoneLabel      = "topology.kubernetes.io/zone"
-	failureDomainZoneLabel = "failure-domain.beta.kubernetes.io/zone"
+	awsKubeConfigClusterContextPrefix    = "arn:"
+	awsKubeConfigClusterContextDelimiter = "/"
+	gcpKubeConfigClusterContextPrefix    = "gke_"
+	gcpKubeConfigClusterContextDelimiter = "_"
 
 	// Region labels
-	topologyRegionLabel      = "topology.kubernetes.io/region"
-	failureDomainRegionLabel = "failure-domain.beta.kubernetes.io/region"
-
-	// Spot/preemptible instance labels
-	nodeInstanceLifecycleLabel = "node.kubernetes.io/instance-lifecycle"
+	topologyRegionLabel = "topology.kubernetes.io/region"
 )
 
-// Node type constants
-const (
-	NodeTypeRegular = "Regular"
-	NodeTypeSpot    = "Spot"
-)
-
-// TODO: for generic provider we can process node level data to dakr
-
-// GenericProvider implements a fallback provider for when cloud detection fails
+// GenericProvider implements basic cloud provider, cluster name, cluster version and cluster region detection
 type GenericProvider struct {
 	sync.Mutex
 
-	logger     logr.Logger
-	k8sClient  kubernetes.Interface
-	nodeGroups map[NodePoolName]map[string][]string
+	logger          logr.Logger
+	k8sClient       kubernetes.Interface
+	kubeContextName string
 }
 
 // NewGenericProvider creates a new generic provider
-func NewGenericProvider(logger logr.Logger, k8sClient kubernetes.Interface) *GenericProvider {
+func NewGenericProvider(logger logr.Logger, k8sClient kubernetes.Interface, kubeContextName string) *GenericProvider {
 	return &GenericProvider{
-		logger:     logger.WithName("generic-provider"),
-		k8sClient:  k8sClient,
-		nodeGroups: make(map[NodePoolName]map[string][]string),
+		logger:          logger.WithName("generic-provider"),
+		k8sClient:       k8sClient,
+		kubeContextName: kubeContextName,
 	}
 }
 
@@ -80,203 +72,73 @@ func (p *GenericProvider) GetClusterMetadata(ctx context.Context) (map[string]in
 
 	// Create basic metadata
 	metadata := map[string]interface{}{
-		"provider":     "generic",
 		"cluster_name": clusterName,
 	}
 
-	// Try to get some information about the cluster infrastructure
-	nodes, err := p.k8sClient.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
-	if err == nil && len(nodes.Items) > 0 {
-		// Check provider IDs for hints about the infrastructure
-		provider := p.guessProviderFromNodes(nodes.Items)
-		if provider != "" {
-			metadata["detected_infrastructure"] = provider
-			metadata["provider"] = provider
-		}
+	infraRegion := ""
+	infraProvider := ""
 
-		// Extract region/zone hints if available
-		regions := make(map[string]bool)
-		zones := make(map[string]bool)
+	// Get nodes to determine various cloud metadata
+	nodes, err := p.k8sClient.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get nodes to infer cluster metadata %w", err)
+	}
+	if len(nodes.Items) > 0 {
+		// Detect infra provider from node provider IDs
+		detectedProvider := p.getProviderFromNodes(nodes.Items)
+		if detectedProvider != "" {
+			infraProvider = detectedProvider
+		}
 
 		for _, node := range nodes.Items {
-			// Check common region/zone labels
+			// Get region from region labels
 			if region, ok := node.Labels[topologyRegionLabel]; ok {
-				regions[region] = true
-			} else if region, ok := node.Labels[failureDomainRegionLabel]; ok {
-				regions[region] = true
+				infraRegion = region
+				break
 			}
-
-			if zone, ok := node.Labels[topologyZoneLabel]; ok {
-				zones[zone] = true
-			} else if zone, ok := node.Labels[failureDomainZoneLabel]; ok {
-				zones[zone] = true
-			}
-		}
-
-		// Add region/zone info if found
-		if len(regions) > 0 {
-			var regionList []string
-			for region := range regions {
-				regionList = append(regionList, region)
-			}
-			metadata["regions"] = regionList
-			metadata["region"] = regionList[0]
-		}
-
-		if len(zones) > 0 {
-			var zoneList []string
-			for zone := range zones {
-				zoneList = append(zoneList, zone)
-			}
-			metadata["zones"] = zoneList
 		}
 	}
 
-	p.logger.Info("Collected generic cluster metadata", "cluster", clusterName)
+	p.logger.Info("Collected generic cluster metadata",
+		zap.String("cluster_name", clusterName),
+		zap.String("provider", infraProvider),
+		zap.String("region", infraRegion),
+	)
+
+	metadata["provider"] = infraProvider
+	metadata["region"] = infraRegion
 
 	return metadata, nil
 }
 
-func (p *GenericProvider) GetNodeGroupMetadata(ctx context.Context) map[string]map[string][]string {
-	nodeList, err := p.k8sClient.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
-	if err != nil { // failed to call K8s API
-		// return cached info, if available
-		return genericTypesToGeneric(p.nodeGroups)
-	}
-
-	refreshedNodeGroupMetadata := getGenericNodeGroupMetadata(nodeList)
-	p.Lock()
-	p.nodeGroups = refreshedNodeGroupMetadata
-	p.Unlock()
-
-	return genericTypesToGeneric(p.nodeGroups)
-}
-
-func getGenericNodeGroupMetadata(nodeList *corev1.NodeList) map[NodePoolName]map[string][]string {
-	nodeGroupInfo := make(map[NodePoolName]map[string][]string)
-	if nodeList == nil {
-		return nodeGroupInfo
-	}
-
-	for _, node := range nodeList.Items {
-		nodePoolLabel := getGenericNodePoolLabel(&node)
-		nodeType := getGenericNodeType(node)
-
-		if _, ok := nodeGroupInfo[nodePoolLabel]; !ok {
-			nodeGroupInfo[nodePoolLabel] = make(map[string][]string)
-		}
-
-		if _, ok := nodeGroupInfo[nodePoolLabel][nodeType]; !ok {
-			nodeGroupInfo[nodePoolLabel][nodeType] = []string{}
-		}
-		nodeGroupInfo[nodePoolLabel][nodeType] = append(nodeGroupInfo[nodePoolLabel][nodeType], node.Name)
-	}
-	return nodeGroupInfo
-}
-
-func getGenericNodePoolLabel(node *corev1.Node) NodePoolName {
-	// In generic there is not consistent node group label unlike providers
-	// Try to determine a meaningful node group name from various common labels
-	// Order by preference: explicit role labels, instance type, zone
-
-	// Check for common role labels
-	roleLabelCandidates := []string{
-		nodeRoleWorkerLabel,
-		nodeRoleMasterLabel,
-		kubernetesRoleLabel,
-		nodeInstanceGroupLabel,
-		nodeGroupLabel,
-	}
-
-	for _, labelKey := range roleLabelCandidates {
-		if val, ok := node.Labels[labelKey]; ok && val != "" {
-			return NodePoolName(val)
-		}
-	}
-
-	if instanceType, ok := node.Labels[nodeInstanceTypeLabel]; ok {
-		return NodePoolName(instanceType)
-	} else if instanceType, ok := node.Labels[nodeBetaInstanceTypeLabel]; ok {
-		return NodePoolName(instanceType)
-	}
-
-	if zone, ok := node.Labels[topologyZoneLabel]; ok {
-		return NodePoolName("zone-" + zone)
-	} else if zone, ok := node.Labels[failureDomainZoneLabel]; ok {
-		return NodePoolName("zone-" + zone)
-	}
-
-	return NodePoolName("default-pool")
-}
-
-func getGenericNodeType(node corev1.Node) string {
-	// Check for common spot/preemptible labels across different providers
-	spotLabelCandidates := []string{
-		nodeInstanceLifecycleLabel,
-		azureScalePriorityLabel,
-		awsCapacityTypeLabel,
-		gkeSpotLabel,
-	}
-
-	for _, labelKey := range spotLabelCandidates {
-		if val, ok := node.Labels[labelKey]; ok {
-			if val == "spot" || val == "Spot" || val == "SPOT" || val == "true" {
-				return NodeTypeSpot
-			}
-		}
-	}
-
-	return NodeTypeRegular
-}
-
-func genericTypesToGeneric(metadata map[NodePoolName]map[string][]string) map[string]map[string][]string {
-	result := make(map[string]map[string][]string)
-	if metadata == nil {
-		return result
-	}
-
-	for nodePool, nodePoolTypeToNodeList := range metadata {
-		result[string(nodePool)] = make(map[string][]string)
-		for nodePoolType, nodeList := range nodePoolTypeToNodeList {
-			result[string(nodePool)][nodePoolType] = nodeList
-		}
-	}
-	return result
-}
-
 // discoverClusterName attempts to determine a cluster name
 func (p *GenericProvider) discoverClusterName(ctx context.Context) (string, error) {
-	// Try to get from kube-system namespace UID - fairly unique
+	if p.kubeContextName != "" {
+		switch {
+		case strings.HasPrefix(p.kubeContextName, awsKubeConfigClusterContextPrefix) && len(strings.Split(p.kubeContextName, awsKubeConfigClusterContextDelimiter)) >= 2:
+			splits := strings.Split(p.kubeContextName, awsKubeConfigClusterContextDelimiter)
+			return splits[len(splits)-1], nil
+		case strings.HasPrefix(p.kubeContextName, gcpKubeConfigClusterContextPrefix) && len(strings.Split(p.kubeContextName, gcpKubeConfigClusterContextDelimiter)) >= 2:
+			splits := strings.Split(p.kubeContextName, gcpKubeConfigClusterContextDelimiter)
+			return splits[len(splits)-1], nil
+		default:
+			return p.kubeContextName, nil
+		}
+	}
+
+	// Try to get from kube-system namespace UID
 	ns, err := p.k8sClient.CoreV1().Namespaces().Get(ctx, "kube-system", metav1.GetOptions{})
 	if err == nil {
 		return fmt.Sprintf("cluster-%s", string(ns.UID)[:8]), nil
 	}
 
-	// Try to get from cluster configuration
-	kubeadmCM, err := p.k8sClient.CoreV1().ConfigMaps("kube-system").Get(ctx, "kubeadm-config", metav1.GetOptions{})
-	if err == nil {
-		if clusterConfig, ok := kubeadmCM.Data["ClusterConfiguration"]; ok {
-			if strings.Contains(clusterConfig, "clusterName:") {
-				lines := strings.Split(clusterConfig, "\n")
-				for _, line := range lines {
-					if strings.Contains(line, "clusterName:") {
-						parts := strings.SplitN(line, ":", 2)
-						if len(parts) > 1 {
-							return strings.TrimSpace(parts[1]), nil
-						}
-					}
-				}
-			}
-		}
-	}
-
 	return "", fmt.Errorf("could not determine cluster name")
 }
 
-// guessProviderFromNodes tries to identify the infrastructure provider from node metadata
-func (p *GenericProvider) guessProviderFromNodes(nodes []corev1.Node) string {
+// getProviderFromNodes identifies infrastructure provider from node metadata
+func (p *GenericProvider) getProviderFromNodes(nodes []corev1.Node) string {
 	if len(nodes) == 0 {
-		return ""
+		return providerNameUnknown
 	}
 
 	// Check provider IDs for common patterns
@@ -284,34 +146,16 @@ func (p *GenericProvider) guessProviderFromNodes(nodes []corev1.Node) string {
 		providerID := node.Spec.ProviderID
 
 		switch {
-		case strings.HasPrefix(providerID, "aws://"):
-			return "aws"
-		case strings.HasPrefix(providerID, "gce://"):
-			return "gcp"
-		case strings.HasPrefix(providerID, "azure://"):
-			return "azure"
-		case strings.HasPrefix(providerID, "openstack://"):
-			return "openstack"
-		case strings.HasPrefix(providerID, "digitalocean://"):
-			return "digitalocean"
-		}
-	}
-
-	// Check for common provider-specific labels
-	for _, node := range nodes {
-		// AWS EKS specific labels
-		if _, ok := node.Labels[awsNodeGroupLabel]; ok {
-			return "aws"
-		}
-
-		// GCP GKE specific labels
-		if _, ok := node.Labels[gkeNodePoolLabel]; ok {
-			return "gcp"
-		}
-
-		// Azure AKS specific labels
-		if _, ok := node.Labels[azureClusterLbel]; ok {
-			return "azure"
+		case strings.HasPrefix(providerID, nodeProviderPrefixAws):
+			return providerNameAws
+		case strings.HasPrefix(providerID, nodeProviderPrefixGcp):
+			return providerNameGcp
+		case strings.HasPrefix(providerID, nodeProviderPrefixAzure):
+			return providerNameAzure
+		case strings.HasPrefix(providerID, nodeProviderPrefixOpenstack):
+			return providerNameOpenstack
+		case strings.HasPrefix(providerID, nodeProviderPrefixDigitalocean):
+			return providerNameDigitalocean
 		}
 	}
 
