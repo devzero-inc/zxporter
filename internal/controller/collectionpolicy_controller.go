e9aff2c16a10902ea7bd6d6a92b8ced1b0f1b8e7
tzvonimir <zvonimir.tomesic@gmail.com>
2025-12-10 15:29:33 +0100

    Restrict to specific resources for secret exchange

--- internal/controller/collectionpolicy_controller.go
+++ internal/controller/collectionpolicy_controller.go
@@ -171,7 +171,8 @@ type PolicyConfig struct {
 //+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;update
 
 // Secret access for cluster token persistence when useSecretForToken is enabled
-//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;create;update;patch
+//+kubebuilder:rbac:groups="",resources=secrets,resourceNames=devzero-zxporter-token,verbs=get;update;patch
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=create
 
 // Metrics access
 //+kubebuilder:rbac:groups="",resources=nodes/metrics,verbs=get
