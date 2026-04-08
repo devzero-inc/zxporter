c467c79e95799bd5cf2a46e1e55ad7b5ed9d0e66
Parthiba hazra <parthibahazra@gmail.com>
2026-04-08 21:25:36 +0530

    [HPA] broadcast network metrics also for HPA (#346)
    * broadcast network metrics also
    
    * configure prometheus to collect cpu throttle metrics also

--- internal/server/mpa_server.go
+++ internal/server/mpa_server.go
@@ -297,6 +297,9 @@ func (sm *SubscriptionManager) Broadcast(data *collector.ContainerMetricsSnapsho
 				MemoryLimitBytes:   data.MemoryLimitBytes,
 				// CPU throttle fraction from CFS bandwidth controller
 				CpuThrottleFraction: data.CpuThrottledFraction,
+				// Network metrics (bytes/sec from Prometheus rate queries)
+				NetworkReceiveBytesPerSec:  int64(data.NetworkReceiveBytes),
+				NetworkTransmitBytesPerSec: int64(data.NetworkTransmitBytes),
 			}
 
 			batch := &gen.ContainerMetricsBatch{
